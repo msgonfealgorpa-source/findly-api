06dd5bd260650adc4eeb86053a83b0811a668874
Talha Muhammad Suleiman <msgonfealgorpa@gmail.com>
2026-01-23 04:31:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 10000;
-
+app.use(cors({ origin: '*' })); // هذا السطر يسمح لموقعك بالوصول للبيانات
 // التعديل هنا: السماح الكامل لكل المصادر لحل مشكلة "فشل الاتصال"
 app.use(cors({
     origin: '*',

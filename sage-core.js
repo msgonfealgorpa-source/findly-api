8a990c584f206eaa6245f52f78b3306c7df6312c
Talha Muhammad Suleiman <msgonfealgorpa@gmail.com>
2026-02-04 23:48:06 +0200

    Update sage-core.js

--- sage-core.js
+++ sage-core.js
@@ -1,1 +1,36 @@
 
+const priceEngine = require("./intelligence/price.engine");
+const timingEngine = require("./intelligence/timing.engine");
+const trustEngine = require("./intelligence/trust.engine");
+const valueEngine = require("./intelligence/value.engine");
+const decisionEngine = require("./intelligence/decision.engine");
+
+function SageCore(product, allProducts) {
+  const prices = allProducts
+    .map(p => Number(p.price))
+    .filter(p => !isNaN(p));
+
+  const price = Number(product.price);
+
+  const priceIntel = priceEngine(price, prices);
+  const timingIntel = timingEngine(priceIntel);
+  const trustIntel = trustEngine(product, priceIntel);
+  const valueIntel = valueEngine(priceIntel, trustIntel);
+
+  const finalVerdict = decisionEngine({
+    priceIntel,
+    timingIntel,
+    trustIntel,
+    valueIntel
+  });
+
+  return {
+    priceIntel,
+    timingIntel,
+    trustIntel,
+    valueIntel,
+    finalVerdict
+  };
+}
+
+module.exports = SageCore;

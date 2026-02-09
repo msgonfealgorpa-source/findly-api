f5b4f36c48991c024a48769f7c151c9f9ff76115
Talha Muhammad Suleiman <msgonfealgorpa@gmail.com>
2026-02-09 17:22:32 +0200

    Update SearchCache.js

--- SearchCache.js
+++ SearchCache.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
 
+const SearchCacheSchema = new mongoose.Schema({
+  query: { type: String, index: true },
+  lang: String,
+  results: Array,
+  createdAt: { type: Date, default: Date.now }
+});
+
+// حذف تلقائي بعد 48 ساعة
+SearchCacheSchema.index(
+  { createdAt: 1 },
+  { expireAfterSeconds: 60 * 60 * 48 }
+);
+
+module.exports = mongoose.model('SearchCache', SearchCacheSchema);
